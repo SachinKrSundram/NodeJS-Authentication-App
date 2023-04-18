3439e22bd50912ee02b189b75c1ed253121e8f71
SachinKrSundram <sachinkumarsundram@gmail.com>
2023-04-18 15:12:06 +0530

    Authentication

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to Database'))
-  .catch(err => console.log('Error connecting to database', err));
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Could not connect to MongoDB', err));
 
 
 
-
+  
 // const mongoose = require('mongoose');
 
 // mongoose.connect('mongodb://127.0.0.1/node.js');
